5ce9c75646e0dedd2068cbbb9086ec4beda9b1d3
Souvik1991 <souvik.maity91@gmail.com>
2013-07-21 20:57:26 +0530

    Create welcome.php

---
+++ welcome.php
@@ -0,0 +1,131 @@
+<!DOCTYPE html>
+<HTML>
+ <HEAD>
+  <TITLE> Welcome to GaanaSuno </TITLE>
+   <link rel="shortcut icon" href="http://farm3.staticflickr.com/2845/9274536857_65a2c41437_o.png" >
+   <style type="text/css">
+    #wrapper
+   {
+   width:1024px;
+   margin:auto;
+   }
+      #header
+      {
+  	background-color:#45AAC7;
+		padding: 9px 65px;
+		text-decoration:none;
+		color:white;
+		font-size:xx-large;
+		font-family:Segoe UI Light;
+		font-weight:bold;
+		opacity:0.7;
+		text-shadow: 0 1px 1px rgba(0, 0, 0, 0.5);
+	  }
+		#center
+		{
+			background:url(http://farm4.staticflickr.com/3816/9269051321_616cc9f718_o.jpg);
+			opacity:0.7;
+			width:100%;
+			height:576px;
+            -webkit-border-radius: 14px;
+    		-moz-border-radius: 14px;
+			-o-border-radius: 14px;
+			border-radius: 14px;
+		}
+			#tb_bt
+			{
+				width:40%;
+				background-color:#45AAC7;
+				padding: 10px 10px 12px 10px;
+				color:white;
+				font-size:x-large;
+				-webkit-border-radius: 14px;
+				-moz-border-radius: 14px;
+				-o-border-radius: 14px;
+				border-radius: 14px;
+				text-shadow: 0 1px 1px rgba(0, 0, 0, 0.5);
+                box-shadow: 3px 3px 3px rgba(0, 0, 0, 0.5);
+			}
+				#tb_bt:hover
+			{
+				width:50%;
+				background-color:#45AAC7;
+				padding: 10px 10px 12px 10px;
+				color:white;
+				font-size:xx-large;
+				text-shadow: 0 2px 2px rgba(0, 0, 0, 0.5);
+                box-shadow: 0 5px 5px rgba(0, 0, 0, 0.5);
+			}
+				#tb_th
+				{
+					width:22%;
+					background-color:#7373AB;
+					padding: 10px 10px 12px 10px;
+					color:white;
+					font-size:xx-large;
+					-webkit-border-radius: 14px;
+					-moz-border-radius: 14px;
+					-o-border-radius: 14px;
+					border-radius: 14px;
+					text-shadow: 0 1px 1px rgba(0, 0, 0, 0.5);
+                    box-shadow: 3px 3px 3px rgba(0, 0, 0, 0.5);
+				}
+			   body
+			  {
+			   background:url(http://farm3.staticflickr.com/2883/9269045267_9f5aa6aac3_o.png);
+			   font-family:Segoe UI Light;
+			   margin:0; 
+			   padding:0;
+			  }
+			  #footer
+			  {
+				background-color:black;
+				opacity:0.7;
+				text-shadow: 0 1px 1px rgba(0, 0, 0, 0.5);
+			  }
+				  a,h3
+				  {
+					text-decoration:none;
+					color:white;
+					text-shadow: 0 1px 1px rgba(0, 0, 0, 0.5);
+				  }
+  </style>
+ </HEAD>
+
+ <BODY>
+		<div id="wrapper">
+		<div id="header">
+		<a href=welcome.php><img src="http://farm4.staticflickr.com/3738/9269051727_4fcc97ae97_o.png" height='3%' width='3%'/>&nbsp;GaanaSuno</a>
+	</div>
+	<br>
+			<div id="center">
+				<CENTER>
+				<br><br><br><br><br>
+					<TABLE>
+					<TR>
+						<TH align=center valign=middle width=1024><div id=tb_th>Listen Song By
+						</div><br><br></TH>
+					</TR>
+					<TR>
+						<TD align=center valign=middle width=1024><a href="choosemood.php"><div id=tb_bt>Selecting Your Mood</div></a>
+						<br><br><br></TD>
+					</TR>
+					<TR>
+						<TD align=center valign=middle width=1024><a href="showalbum.php"><div id=tb_bt>Selecting Movie Album</div></a>
+						<br><br><br></TD>
+					</TR>
+					</TABLE>
+				</CENTER>
+			</div>
+			<br>
+				<div id="footer">
+					<TABLE cellspacing=2 width=100%>
+					<TR>
+						<TD><h3>&nbsp; &nbsp; &copy; Souvik Maity Product 2013.</h3></TD>
+    					<TD align='right'><h3>Contact Me at: &nbsp; &nbsp; </h3><a href="http://www.facebook.com/amisouvik91"><img src="http://farm6.staticflickr.com/5442/9269051605_da5e7f45c3_o.png" height="32" width="32" title="Facebook"></a>&nbsp; &nbsp; <a href="#"><img src="http://farm4.staticflickr.com/3810/9271834062_f0390cc182_o.png"  height="32" width="32" title="Twitter" /></a>&nbsp; &nbsp; <a href="#"><img src="http://farm8.staticflickr.com/7365/9269051849_64162c343c_o.png"  height="32" width="32" title="gmail"></a>&nbsp; &nbsp; &nbsp; &nbsp;</TD>
+					</TR>
+					</TABLE>
+				</div>
+		</div>
+ </BODY>
+</HTML>
